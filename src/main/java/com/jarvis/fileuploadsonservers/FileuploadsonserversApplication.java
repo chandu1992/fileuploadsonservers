769c7b14a4778ac95989c7fe8fa88d9d93054b7f
chandrashekharbhaupande <chpande55@gmail.com>
2026-02-13 21:18:05 +0530

    file upload on azure blob storage

---
+++ src/main/java/com/jarvis/fileuploadsonservers/FileuploadsonserversApplication.java
@@ -0,0 +1,13 @@
+package com.jarvis.fileuploadsonservers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadsonserversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadsonserversApplication.class, args);
+	}
+
+}
